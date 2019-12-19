867a8db42c7c743b9be3f41e4727c229973f8074
Semseddin AKSOY <semsaksoy@gmail.com>
2019-12-19 13:29:42 +0300

    first

---
+++ device.rb
@@ -0,0 +1,103 @@
+class Device
+  @vdom=nil
+
+  def initialize(ssh, vdom)
+    @ssh=ssh
+    @vdom="config vdom\n edit #{vdom}\n" unless vdom.nil?
+  end
+
+  def rules
+    t=[]
+    p_info= @ssh.exec!("#{@vdom}show firewall policy")
+    policy_list=p_info.scan(/edit.*?next/m).flatten
+    policy_list.each_with_index do |p, i|
+      p_info=p
+      name=grep(p_info, /edit\s(.*?)$/)
+      s_info= @ssh.exec!("#{@vdom}diagnose firewall iprope show 100004 #{name}")
+      rule=name
+      sintf=grep(p_info, /set\ssrcintf\s(.*?)$/)
+      dintf=grep(p_info, /set\sdstintf\s(.*?)$/)
+      saddr=grep(p_info, /set\ssrcaddr\s(.*?)$/)
+      daddr=grep(p_info, /set\sdstaddr\s(.*?)$/)
+      groups=grep(p_info, /set\sgroups\s(.*?)$/)
+      service=grep(p_info, /set\sservice\s(.*?)$/)
+      nat=grep(p_info, /set\snat\s(.*?)$/)
+      action=grep(p_info, /set\saction\s(.*?)\"?$/)||"deny"
+      comments=grep(p_info, /set\scomments\s(.*?)$/)
+      status=grep(p_info, /set\sstatus\s(.*?)$/)||"enable"
+      pkt=grep(s_info, /pkts\/bytes\=(.*?)\s/)
+      cnt=grep(s_info, /hit\scount\:(.*?)\s/)||0
+      t<<{order: i+1, rule: rule, action: action, sintf: sintf, dintf: dintf, saddr: saddr, daddr: daddr,
+          groups: groups, service: service, nat: nat, comments: comments, status: status, pkt: pkt, cnt: cnt, }
+    end
+    rules= t
+  end
+
+  def nats
+    t=[]
+    p_info= @ssh.exec!("#{@vdom}show firewall vip")
+    policy_list=p_info.scan(/edit.*?next/m).flatten
+    policy_list.each_with_index do |p, i|
+      name=grep(p, /edit\s(.*?)$/)
+      extip=grep(p, /set\sextip\s(.*)/)
+      mappedip=grep(p, /set\smappedip\s\"(.*)\"/)
+      extport=grep(p, /set\sextport\s(.*)/)
+      mappedport=grep(p, /set\smappedport\s(.*)/)
+      t<<{order: i+1, name: name, extip: extip, mappedip: mappedip, extport: extport, mappedport: mappedport}
+    end
+    nats=t
+  end
+
+  def user_groups
+    t=[]
+    u_info= @ssh.exec!("#{@vdom}show user group")
+    user_group_list=u_info.scan(/edit.*?next/m).flatten
+    user_group_list.each_with_index do |p, i|
+      name=grep(p, /edit\s(.*?)$/)
+      member=grep(p, /set\smember\s(.*)/)
+      group=grep(p, /set\sgroup-name\s(.*)/)
+      t<<{order: i+1, name: name, member: member, group: group}
+    end
+    user_groups=t
+  end
+
+  def users
+    t=[]
+    u_info= @ssh.exec!("#{@vdom}show user local")
+    user_list=u_info.scan(/edit.*?next/m).flatten
+    user_list.each_with_index do |p, i|
+      name=grep(p, /edit\s(.*?)$/)
+      type=grep(p, /set\stype\s(.*)/)
+      type="local" if type=="password"
+      status=grep(p, /set\sstatus\s(.*?)$/)||"enable"
+      t<<{order: i+1, name: name, type: type, status: status}
+    end
+    users=t
+  end
+
+  def addressgroup
+    t=[]
+    u_group= @ssh.exec!("#{@vdom}show firewall addrgrp")
+    group_list=u_group.scan(/edit.*?next/m).flatten
+    group_list.each_with_index do |p, i|
+      name=grep(p, /edit\s(.*?)$/)
+      member=grep(p, /set\smember\s(.*)/)
+
+      t<<{order: i+1, name: name, member: member}
+    end
+    addressgroup=t
+  end
+
+  def servicegroup
+    t=[]
+    u_service= @ssh.exec!("#{@vdom}show firewall service group")
+    service_list=u_service.scan(/edit.*?next/m).flatten
+    service_list.each_with_index do |p, i|
+      name=grep(p, /edit\s(.*?)$/)
+      member=grep(p, /set\smember\s(.*)/)
+      t<<{order: i+1, name: name, member: member}
+    end
+    servicegroup=t
+  end
+
+end
---
+++ helper.rb
@@ -0,0 +1,45 @@
+require 'bundler/inline'
+gemfile do
+  source 'https://rubygems.org'
+  gem "parallel"
+  gem "net-ssh"
+  gem "axlsx"
+  gem "mail"
+  gem "ruby-progressbar"
+end
+
+require "net/ssh"
+require 'net/ssh/proxy/errors'
+require 'net/ssh/proxy/socks5'
+require "axlsx"
+require "json"
+require "date"
+require "ruby-progressbar"
+require "mail"
+require "parallel"
+
+
+##config
+@concurency=8
+@mail_server="localhost"
+@mail_port=25
+@mail_from="fortireport@domain.com"
+@mail_subject="forti report"
+@gsni = Net::SSH::Proxy::SOCKS5.new('158.98.136.75',1080, :user => 'TRZZ', :password => '')
+##config
+
+Mail.defaults do
+  delivery_method :smtp, address: @mail_server, port: @mail_port
+end
+
+require_relative("device")
+
+system("clear")
+print "Chakybay 2.0\nlinkedin.com/in/semsaksoy\n"
+print "\n\n"
+
+
+def grep(str, regex)
+  get=str.match(regex)[1].strip.gsub("\"", "") if str.match(regex)
+end
+
